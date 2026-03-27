29a61b948fc222d3abaa40d0680a424940c9abec
ksmushkov-cmd <ksmushkov@bk.ru>
2026-03-27 14:42:56 +0700

    вот мои 8 строчек

---
+++ Group1/Smushkov.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForkKnights.Group1 {
+  public class Smushkov : IKnight {
+
+    public string Archetype { get; set; }
+    public string Name { get; set; }
+    public string Superpower { get; set; }
+    public double RealityDistortionPercent { get; set; }  // Мощность искажения реальности
+    public int QuantumCoherenceMinutes { get; set; }      // Длительность действия
+    public int NeuralChipVersion { get; set; }            // Версия нейрочипа
+    public bool IsLicensed { get; set; }                  // Лицензия на использование суперсилы
+    public List<string> QuantumAnomalies { get; set; }    // Зафиксированные квантовые аномалии
+
+    public Smushkov() {
+
+      Archetype = "Cyber Detective";
+      HasLicense = true;
+
+      PastYearLicenseList = new List<string> { "2517", "2312", "2099" };
+      PastYearLicenseList.Add("2075");
+      Name = "Декард-2077";
+      Specialization = "идентификация репликантов, работа с нейросетями класса «Оракул»";
+      AccuracyRate = 0.003;
+      NeuroDeckCharge = 42;
+    }
+
+    public string GetJobApplication() {
+
+    }
+
+  }
+}
